4be37f9cdae3647fcb0a262e956db6075fdb3e49
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-09-29 16:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+  if (s === "") return ""; // handle empty string
+
+  s = s.trimStart(); // remove leading spaces
+
+  let spaceIndex = s.indexOf(" ");
+  if (spaceIndex === -1) {
+    return s; // no space → whole string
+  }
+  return s.slice(0, spaceIndex); // return part before first space
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
